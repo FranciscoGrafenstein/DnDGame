fc0fffee924c486ec22666667d37dbf6add6872e
francisco.acosta <francisco.acosta@globant.com>
2022-04-07 22:46:05 -0300

    deleted usless creatures

--- src/Training.java
+++ src/Training.java
@@ -11,13 +11,9 @@ public class Training {
     public static void main(String[] args) throws IOException {
 
         Player character = new Player("Pancho", PlayerClasses.MAGE);
-        Creature firstCreature = new Creature(Creatures.DRAUGR, 30, Levels.LEVEL_2, 25, 35, 15);
-        Creature secondCreature = new Creature(Creatures.VAMPIRE, 40, Levels.LEVEL_3, 25, 10, 15);
-        Creature thirdCreature = new Creature(Creatures.ZOMBIE, 30, Levels.LEVEL_2, 25, 10, 15);
         character.characterCreationSystem();
         Magic.addSpell(character, Spells.FIRE_BALL);
         Magic.addSpell(character, Spells.ICE_BALL);
-
         Items.addItemBag(character, ItemList.HP_POTY, 2);
         Items.addItemBag(character, ItemList.MP_POTY, 1);
 
